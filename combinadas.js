4b1e7c3013d5fad70827fd4426f2a6ac1ab225fd
julimiguens88 <miguens.julia@gmail.com>
2023-07-19 23:52:24 -0300

    subo funcion de combinadas

---
+++ combinadas.js
@@ -0,0 +1,23 @@
+let numeroPorDos = function (numero) {
+    return numero * 2
+}
+
+
+function esPar(numero){
+    if(numero%2 == 0){
+        return true;
+    }
+    return false;
+};
+
+function paresDuplicados(numero) {
+    let array = [];
+    for (let i = 0; i < numero.length; i++) {
+        if (esPar(numero[i])) {
+            array.push(numeroPorDos(numero[i]))
+        }
+    }
+    return array;
+}
+
+console.log(paresDuplicados([1, 2, 3, ,4 ,5 , 6 , 7, 8, 9, 10]));
